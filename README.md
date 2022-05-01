4492c3d258e9c3175d5889c2c9f827a99a8fda51
Cecilia Glynn <ceciliaglynn@icloud.com>
2022-04-30 21:01:12 -0700

    Working API, added 5 day forcast, local storage saves cities

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # VI-Weather-App
 Weather Application using the weather API
+
+Users will be able to enter a city
+API call to OpenWeather is made
+Will retrieve current weather and 5 day forecast
+City will also be saved to local storage and will be stored upon refresh
+
+![splash-page](assets/images/FinalSite.png)
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,23 @@
 var submitButton = document.querySelector("#submitButton");
 var searchField = document.querySelector("#searchField");
 var statContainer = document.querySelector("#statContainer");
+var appendCities = document.querySelector("#appendCities");
+var forecastContainer = document.querySelector("#forecastContainer");
 var openWeatherAPIKey = "dd30bd6b4179fbaa5f03952d07085a18";
 var cityName;
 var temperature;
+var listOfCities = [];
+
+function initiate(){
+  if (localStorage.getItem("savedCity")){
+    listOfCities = JSON.parse(localStorage.getItem("savedCity"))
+  }
+  for (let city of listOfCities){
+    appendCities.innerHTML += `<button>${city}</button`;
+  }
+}
+
+initiate();
 
 function getCurrentWeather(location, lat, lon) {
   // & is a different parameter
@@ -19,6 +33,9 @@ function getCurrentWeather(location, lat, lon) {
       // console.log(data.main.humidity);
       // console.log(data.main.temp);
       // console.log(data.wind.speed);
+
+      statContainer.innerHTML = "";
+
       var createName = document.createElement("h2"); //Create the element
       createName.textContent = data.name; // Gave element value
       statContainer.appendChild(createName);
@@ -45,26 +62,57 @@ function getCurrentWeather(location, lat, lon) {
         .then((response) => {
           return response.json();
         })
-        .then((data) => {
-          console.log(data);
+        .then((forecast) => {
+          console.log(forecast);
 
           var writeUVI = document.createElement("p");
-          writeUVI.textContent = "UV Index: " + data.current.uvi;
+          writeUVI.textContent = "UV Index: " + forecast.current.uvi;
           statContainer.appendChild(writeUVI);
-
-          for (i = 0; i <= 5; i++) {
-            var htmlTemplate = document.createElement("ul");
-            forecastContainer.appendChild(htmlTemplate);
-          }
+          display5Forecast(forecast);
         });
+
+        if(!duplicateCheck(data.name)){
+          return;
+        }
+        saveToLocal(data.name);
+        appendCities.innerHTML += `<button>${searchField.value}</button`;
+
     });
-}
+};
 
 submitButton.addEventListener("click", function (event) {
   console.log(searchField.value);
 
   getCurrentWeather(searchField.value);
 });
+
+function saveToLocal(city){
+  listOfCities.push(city);
+  localStorage.setItem("savedCity", JSON.stringify(listOfCities));
+  console.log(city);
+}
+
+function duplicateCheck(city){
+  for (var i=0; i <listOfCities.length; i++){
+    if (listOfCities[i].toLowerCase() === city.toLowerCase()){
+      return false;
+    } 
+  } return true;
+}
+
+function display5Forecast(moreData){
+  forecastContainer.innerHTML = "";
+  for (var i=0; i <5; i++){
+    var unixCode = new Date(moreData.daily[i].dt * 1000); //Generates the date in unixcode * 1000 milliseconds
+    var theDate = unixCode.toDateString();
+    forecastContainer.innerHTML += `<div class="forecast-card">
+                                    <h4>${theDate}</h4>
+                                    <img src="https://openweathermap.org/img/w/${moreData.daily[i].weather[0].icon}.png">
+                                    <h3>${moreData.daily[i].temp.day}</h3>
+                                    <h3>${moreData.daily[i].humidity}</h3>
+                                    </div>`
+  }
+}
 
 // Function that takes in lat and long
 // Fetches new API
